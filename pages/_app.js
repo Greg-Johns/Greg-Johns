29359e760519d29663aacfd2ce657849b3661cb5
Greg Johns <greg.johns@cbre.com>
2023-02-13 23:21:13 -0600

    clean out

--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-import '../styles/globals.css';
-
-export default function Nextra({ Component, pageProps }) {
-  return (
-    <main className="mainGrid">
-      <Component {...pageProps} />
-    </main>
-  )};
