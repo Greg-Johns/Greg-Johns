d89bac3dbccc7272b1e70991ccf7283707b5a9a1
Greg Johns <greg.johns@cbre.com>
2023-04-23 20:43:55 -0500

    added Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,11 @@
-/* import type { AppProps } from 'next/app'; */
-/* import { Analytics } from '@vercel/analytics/react'; */
+import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
 import './globals.css'
 import Header from './Header'
 import Footer from './Footer'
 //import error from './error'
 //import ErrorBoundary from 'react'
 
-/* export default function RootLayout({ children }: AppProps) { */
 export default function RootLayout({ children }: {
   children: React.ReactNode
 }) {
@@ -21,6 +20,7 @@ export default function RootLayout({ children }: {
             </div>
           <Footer />
         </main>
+        <Analytics />
       </body>
     </html>
   )
