ea1976f744cb4bf762ba9f19162d04c8542f3c09
Greg Johns <greg.johns@cbre.com>
2023-11-19 14:30:26 -0600

    good save spot

--- src/app/stuff/page.tsx
+++ src/app/stuff/page.tsx
@@ -3,22 +3,18 @@ import Link from 'next/link';
 import '../comics.css'
 
 import hero from '../../../public/stuff/heroes_villians/hv_9.jpg'
-import sketchbook from '../../../public/stuff/sketchbooks/sb_15.jpg'
-import sites from '../../../public/stuff/tn_sites.gif'
-import marks from '../../../public/stuff/tn_mark.jpg'
-import cartoons from '../../../public/stuff/cartoons/spock.jpg'
-import screens from '../../../public/stuff/tn_screens.jpg'
 import ng1 from '../../../public/genart/ng/ng12.jpg'
 import ng2 from '../../../public/genart/waves/waves_12.jpg'
+import ng3 from '../../../public/genart/spirals/spiral_17.jpg'
+import sketchbook1 from '../../../public/stuff/sketchbook1/sb_15.jpg'
+import sketchbook2 from '../../../public/stuff/sketchbook1/sb_19.jpg'
+import sites from '../../../public/stuff/tn_sites.gif'
+import marks from '../../../public/stuff/tn_mark.jpg'
+import blkbrd from '../../../public/stuff/blackboard/flyball.jpeg'
+import cartoons from '../../../public/stuff/cartoons/spock.jpg'
+import screens from '../../../public/stuff/tn_screens.jpg'
 
 export default function Stuff() {
-
-  var sectionStyle = {
-    border: "4px solid red",
-    //backgroundImage: `url(${hero})`
-    backgroundImage: `url(${hero})`
-  };
-
 
   return (
     <>
@@ -43,8 +39,11 @@ export default function Stuff() {
             <Link href='/heroes'>
               <div
                 className='hero' 
-/* https://davidwalsh.name/multiple-background-css-animations */
-                style={{ backgroundImage:`url(${JSON.stringify(hero.src)})` }}
+              /* https://davidwalsh.name/multiple-background-css-animations */
+                style={{ 
+                  backgroundImage:`url(${JSON.stringify(hero.src)})`,
+                  /* backgroundPosition: '0 -90px', */
+                }}
               >
                   <details open>
                     <summary>
@@ -64,7 +63,7 @@ export default function Stuff() {
                 <summary>
                   <h5>Node Garden 1</h5>
                 </summary>
-                <p>Here's some stuff about heroes and villians.</p>
+                <p>Here's some stuff about heroes and villians. I want to write more here to see what it looks like.</p>
               </details>
             </div>
           </Link>
@@ -86,7 +85,7 @@ export default function Stuff() {
           <Link href='/heroes'>
             <div
               className='ng3'
-              style={{ backgroundImage:`url(${JSON.stringify(hero.src)})` }}
+              style={{ backgroundImage:`url(${JSON.stringify(ng3.src)})` }}
             >
               <details open>
                 <summary>
@@ -100,7 +99,7 @@ export default function Stuff() {
           <Link href='/heroes'>
             <div 
               className='skbk1'
-              style={{ backgroundImage:`url(${JSON.stringify(sketchbook.src)})` }}
+              style={{ backgroundImage:`url(${JSON.stringify(sketchbook1.src)})` }}
             >
               <details open>
                 <summary>
@@ -110,11 +109,25 @@ export default function Stuff() {
               </details>
             </div>
           </Link>
+
+          <Link href='/heroes'>
+            <div 
+              className='skbk2'
+              style={{ backgroundImage:`url(${JSON.stringify(sketchbook2.src)})` }}
+            >
+              <details open>
+                <summary>
+                  <h5>Sketchbook 2</h5>
+                </summary>
+                <p>Here's some ...</p>
+              </details>
+            </div>
+          </Link>
           
           <Link href='/heroes'>
             <div 
               className='blkbrd'
-              style={{ backgroundImage:`url(${JSON.stringify(hero.src)})` }}
+              style={{ backgroundImage:`url(${JSON.stringify(blkbrd.src)})` }}
             >
               <details open>
                 <summary>
@@ -170,7 +183,7 @@ export default function Stuff() {
           <Link href='/heroes'>
             <div 
               className='skbk2'
-              style={{ backgroundImage:`url(${JSON.stringify(hero.src)})` }}
+              style={{ backgroundImage:`url(${JSON.stringify(sketchbook2.src)})` }}
             >
               <details open>
                 <summary>
